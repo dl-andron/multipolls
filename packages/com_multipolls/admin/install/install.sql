67077f007a532dfc6c57f2c41843bafeff95e59c
dl-andron <43088618+dl-andron@users.noreply.github.com>
2021-02-10 11:40:44 +0300

    Update install.sql

--- packages/com_multipolls/admin/install/install.sql
+++ packages/com_multipolls/admin/install/install.sql
@@ -159,6 +159,18 @@ CREATE TABLE IF NOT EXISTS `#__multipolls_cb_own_votes` (
   KEY `id_question` (`id_question`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
+CREATE TABLE IF NOT EXISTS `#__multipolls_priority_votes` (
+  `id_vote` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_question` int(10) unsigned NOT NULL,
+  `id_answer` int(10) unsigned NOT NULL,
+  `value` int(10) DEFAULT NULL,  
+  `ip` text NOT NULL,
+  `user_agent` text NOT NULL,
+  `date_voting` datetime NOT NULL,
+  PRIMARY KEY (`id_vote`),
+  KEY `id_question` (`id_question`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
 INSERT INTO `#__multipolls_type_questions` (`id`, `type`) VALUES
 (1, 'Один вариант'),
 (2, 'Несколько вариантов'),
